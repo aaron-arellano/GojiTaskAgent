a3849f9b335e796502dbd5db05e91bf9bb2a3b36
Aaron <aaron.ar3llano@gmail.com>
2020-10-22 19:09:24 -0700

    clean comments and make new delete swipe class look cleaner

--- app/src/main/java/com/vt/taskagent/model/TaskLab.java
+++ app/src/main/java/com/vt/taskagent/model/TaskLab.java
@@ -4,9 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
 import com.vt.taskagent.database.TaskDbSchema.*;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
