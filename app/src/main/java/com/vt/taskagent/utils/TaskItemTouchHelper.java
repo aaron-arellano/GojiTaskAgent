dd04a778bc4640f16c1100bb6bf8f2b04c8e6f5e
aaron-arellano <52056643+aaron-arellano@users.noreply.github.com>
2020-10-23 15:11:45 -0700

    Add swipe to delete background color and trash icon (#9)

--- app/src/main/java/com/vt/taskagent/utils/TaskItemTouchHelper.java
+++ app/src/main/java/com/vt/taskagent/utils/TaskItemTouchHelper.java
@@ -1,6 +1,12 @@
 package com.vt.taskagent.utils;
 
+import com.vt.taskagent.R;
 import com.vt.taskagent.view.TaskAdapter;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,9 +19,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class TaskItemTouchHelper extends ItemTouchHelper.Callback {
 
     private TaskAdapter mTaskAdapter;
+    private Context mContext;
+    private ColorDrawable mBackground;
 
-    public TaskItemTouchHelper(TaskAdapter mAdapter) {
+    public TaskItemTouchHelper(TaskAdapter mAdapter, Context context) {
         this.mTaskAdapter = mAdapter;
+        this.mContext = context;
     }
 
     @Override
@@ -30,8 +39,37 @@ public class TaskItemTouchHelper extends ItemTouchHelper.Callback {
         return false;
     }
 
+    // delete the item when we swipe to the right
     @Override
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
         mTaskAdapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+
+
+    // override method to draw red background when deleting
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        Drawable trashIcon = mContext.getDrawable(R.drawable.ic_delete);
+        View v = viewHolder.itemView;
+        int vHeight = v.getBottom() - v.getTop();
+        int trashHeight = trashIcon.getIntrinsicHeight();
+        int trashWidth = trashIcon.getIntrinsicWidth();
+
+        mBackground = new ColorDrawable(mContext.getColor(R.color.colorRed));
+        // set the bounds of the red background within the recycler view viewHolder for the given Task in the list
+        // setBounds (left, top, right, bottom),, dx is the offset of the viewHolder size
+        int top = (int)(v.getTop() + (vHeight*.05));
+        mBackground.setBounds(v.getLeft(), top, v.getRight(), v.getBottom());
+        mBackground.draw(c);
+
+        // draw the trash icon next
+        int trashTop = v.getTop() + (trashHeight/2);
+        int trashBottom = trashTop + trashHeight;
+        int trashLeft = v.getLeft() + (trashWidth/2);
+        int trashRight = trashLeft + trashWidth;
+        trashIcon.setBounds(trashLeft, trashTop, trashRight, trashBottom);
+        trashIcon.draw(c);
     }
 }
